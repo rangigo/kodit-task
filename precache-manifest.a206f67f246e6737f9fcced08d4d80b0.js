9845a51ccf4819f935d0f85554b89dea5f7d18a7
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-11-05 08:39:21 +0200

    Updates

---
+++ precache-manifest.a206f67f246e6737f9fcced08d4d80b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c5530dc77bb4a8f3f",
+    "url": "/kodit-task/static/js/runtime~main.448c5530.js"
+  },
+  {
+    "revision": "6d2e270a053cadf92b7e",
+    "url": "/kodit-task/static/js/main.6d2e270a.chunk.js"
+  },
+  {
+    "revision": "069c4bc770d1b97de353",
+    "url": "/kodit-task/static/js/1.069c4bc7.chunk.js"
+  },
+  {
+    "revision": "6d2e270a053cadf92b7e",
+    "url": "/kodit-task/static/css/main.e0bfe86b.chunk.css"
+  },
+  {
+    "revision": "069c4bc770d1b97de353",
+    "url": "/kodit-task/static/css/1.8fd681b2.chunk.css"
+  },
+  {
+    "revision": "d3d414c32d737657e523cc70f6661fc7",
+    "url": "/kodit-task/index.html"
+  }
+];
